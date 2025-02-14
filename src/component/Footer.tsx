02459115b54974007941ede75611118e7ef7cf9a
JPChoyon <kotjpckhan@gmail.com>
2025-02-15 00:47:59 +0600

    daisyui and tailwind installed, navbar, footer, layout added

---
+++ src/component/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by J P
+            Choyon Khan
+          </p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
